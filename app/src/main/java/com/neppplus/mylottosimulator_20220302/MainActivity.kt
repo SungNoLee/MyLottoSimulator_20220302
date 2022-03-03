359a5aa301c6ec5afa37c178c5775696dbe445cc
이 성 노 <kulbo0926@gmail.com>
2022-03-03 12:51:50 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/mylottosimulator_20220302/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.neppplus.mylottosimulator_20220302
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+    fun setValues() {
+
+    }
+}
